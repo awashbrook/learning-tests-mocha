faf27a4922c3c21c8b7e724145603122047a0c99
awashbrook <awashbrook@gmail.com>
2015-04-20 10:51:44 +0100

    proved that both inheritance patterns are the same sync and async...

--- spec/promises.spec.js
+++ spec/promises.spec.js
@@ -13,80 +13,99 @@ chai.use(sinonChai);
 var log = console;
 
 describe('promises basics', function () {
-  describe('test q', function () {
-    var protoPerson = {
-      _disposition: 'Anticipation',
-      eat: function (food) {
-        this._disposition = 'Satisfied';
-        log.info("\n\n" + this._name + "is eating delicious " + food);
-      },
-      beHungry: function (reason) {
-        this._disposition = 'Starving';
-        log.warn("\n\n" + this._name + "is hungry because: " + reason);
-      },
-      disposition: function () {
-        return this._disposition;
-      }
+  var protoPerson = {
+    _disposition: 'Anticipation',
+    eat: function (food) {
+      this._disposition = 'Satisfied';
+      log.info("\n\n" + this._name + " is eating delicious " + food);
+    },
+    beHungry: function (reason) {
+      this._disposition = 'Starving';
+      log.warn("\n\n" + this._name + " is hungry because: " + reason);
+    },
+    disposition: function () {
+      return this._disposition;
+    }
+  };
+  // TODO can't figure out how to bind this for async promise callbacks invocation of eat, this doesn't exist above?!
+
+  function closurePerson(name, disposition) {
+    disposition = disposition || 'Anticipation';
+    function eat(food) {
+      disposition = 'Satisfied';
+      log.info("\n\n" + name + " is eating delicious " + food);
+    }
+    function beHungry(reason) {
+      disposition = 'Starving';
+      log.warn("\n\n" + name + " is hungry because: " + reason);
+    }
+    function getDisposition() {
+      return disposition;
+    }
+    return { // privileged methods
+      disposition: getDisposition,
+      eat: eat,
+      beHungry: beHungry
     };
-    // TODO can't figure out how to bind this for promise callbacksinvocation of eat, this doesn't exist above?!
+  }
 
-    function closurePerson(name, disposition) {
-      disposition = disposition || 'Anticipation';
-      function eat(food) {
-        disposition = 'Satisfied';
-        log.info("\n\n" + name + " is eating delicious " + food);
-      }
-      function beHungry(reason) {
-        disposition = 'Starving';
-        log.warn("\n\n" + name + " is hungry because: " + reason);
-      }
-      function getDisposition() {
-        return disposition;
-      }
-      return { // privileged methods
-        disposition: getDisposition,
-        eat: eat,
-        beHungry: beHungry
-      };
-    }
+  function testPersonState(person) {
+    expect(person.disposition).to.exist;
+    expect(person.disposition).to.be.a('function');
+    expect(person.disposition()).to.equal('Anticipation');
 
-    function testPersonState(person) {
-      expect(person.disposition).to.exist;
-      expect(person.disposition).to.be.a('function');
-      expect(person.disposition()).to.equal('Anticipation');
+    expect(person.beHungry).to.exist;
+    expect(person.beHungry).to.be.a('function');
+    person.beHungry("Couldn't find take out menu!");
+    expect(person.disposition()).to.equal('Starving');
 
-      expect(person.beHungry).to.exist;
-      expect(person.beHungry).to.be.a('function');
-      person.beHungry("Couldn't find take out menu!");
-      expect(person.disposition()).to.equal('Starving');
+    expect(person.eat).to.exist;
+    expect(person.eat).to.be.a('function');
+    person.eat('Bananas');
+    expect(person.disposition()).to.equal('Satisfied');
+  }
 
-      expect(person.eat).to.exist;
-      expect(person.eat).to.be.a('function');
-      person.eat('Bananas');
-      expect(person.disposition()).to.equal('Satisfied');
-    }
+  it("should show our two person impls are identical in behaviour when called synchronously", function () {
+    var andy;
 
-    it("should show our two person impls are identical in behaviour when called synchronously", function () {
-      var andy;
+    andy = closurePerson('Funky Andy');
+    testPersonState(andy);
 
-      andy = closurePerson('Andy');
-      testPersonState(andy);
+    andy = Object.create(protoPerson);
+    andy._name = 'Object Andy';
+    testPersonState(andy);
+  });
+  it.only("should show our two person impls are identical in behaviour when called asynchronously", function (done) {
+    var funkyAndy = closurePerson('Funky Andy');
+    setInterval(function () {
+      testPersonState(funkyAndy);
+    }, 10);
 
-      andy = Object.create(protoPerson);
-      andy._name = 'Andy';
-      testPersonState(andy);
-    });
+    var objectAndy = Object.create(protoPerson);
+    objectAndy._name = 'Object Andy';
+    setInterval(function () {
+      testPersonState(objectAndy);
+      done();
+    }, 10);
+  });
+
+  var cadey = closurePerson('Cadey');
+  //  this is not bound correctly when promise calls back: Cannot set property '_disposition' of undefined
+  var cadey = Object.create(protoPerson);
+  cadey._name = 'Cadey';
+
+  describe('test q', function () {
 
     it('should illustrate basic q usage', function (done) {
-      expect(andy.disposition()).to.equal('Anticipation');
+      expect(cadey.disposition()).to.equal('Anticipation');
 
       var pizzaOrderFulfillment = q.defer();
       var pizzaDelivered = pizzaOrderFulfillment.promise;
 
-      pizzaDelivered.then(andy.eat, andy.beHungry).done(done);
+      pizzaDelivered.then(cadey.eat, cadey.beHungry).done(done);
       pizzaOrderFulfillment.resolve('Pepperoni');
 
-      expect(andy.disposition()).to.equal('Satisfied');
+      expect(cadey.disposition()).to.equal('Satisfied');
     });
 
     var Restaurant = function () {
@@ -110,7 +129,7 @@ describe('promises basics', function () {
       var pizzaPit = new Restaurant();
       var pizzaDelivered = pizzaPit.takeOrder('Capricciosa');
 
-      pizzaDelivered.then(andy.eat, andy.beHungry);
+      pizzaDelivered.then(cadey.eat, cadey.beHungry);
 
       pizzaPit.problemWithOrder('no Capricciosa, only Margherita');
     });
