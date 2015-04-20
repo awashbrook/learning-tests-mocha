1442cf9dc06f5394ea843318957b369f9b33daa0
awashbrook <awashbrook@gmail.com>
2015-04-20 20:02:24 +0100

    committing with just a couple of tests failing in promises and inheritance

--- spec/promises.spec.js
+++ spec/promises.spec.js
@@ -21,13 +21,13 @@ describe('promises basics', function () {
       this._disposition = 'Satisfied';
       var str = ("\n\n" + this._name + " is eating delicious " + food);
       log.info(str);
-      return str;
+      return str; // return q.resolve() or value
     },
     beHungry: function (reason) {
       this._disposition = 'Starving';
       var str = ("\n\n" + this._name + " is hungry because: " + reason);
       log.info(str);
-      return str;
+      throw new Error(str); // return q.reject() or exception
     },
     disposition: function () {
       return this._disposition;
@@ -60,210 +60,201 @@ describe('promises basics', function () {
       beHungry: beHungry
     };
   }
+  describe("should show our two person impls are identical in behaviour", function () {
+    function testPersonState(person) {
+      expect(person.disposition).to.exist;
+      expect(person.disposition).to.be.a('function');
+      expect(person.disposition()).to.equal('Anticipation');
 
-  function testPersonState(person) {
-    expect(person.disposition).to.exist;
-    expect(person.disposition).to.be.a('function');
-    expect(person.disposition()).to.equal('Anticipation');
+      expect(person.beHungry).to.exist;
+      expect(person.beHungry).to.be.a('function');
+      expect(function () {
+        person.beHungry("Couldn't find take out menu!");
+      }).to.throw(/menu/);
+      expect(person.disposition()).to.equal('Starving');
 
-    expect(person.beHungry).to.exist;
-    expect(person.beHungry).to.be.a('function');
-    person.beHungry("Couldn't find take out menu!");
-    expect(person.disposition()).to.equal('Starving');
-
-    expect(person.eat).to.exist;
-    expect(person.eat).to.be.a('function');
-    person.eat('Bananas');
-    expect(person.disposition()).to.equal('Satisfied');
-  }
-
-  it("should show our two person impls are identical in behaviour when called synchronously", function () {
-    var andy;
-    andy = closurePerson('Funky Andy');
-    testPersonState(andy);
-    andy = Object.create(protoPerson);
-    andy._name = 'Object Andy';
-    testPersonState(andy);
-  });
-  it("should show our two person impls are identical in behaviour when called asynchronously", function (done) {
-    var funkyAndy = closurePerson('Funky Andy');
-    setInterval(function () {
+      expect(person.eat).to.exist;
+      expect(person.eat).to.be.a('function');
+      person.eat('Bananas');
+      expect(person.disposition()).to.equal('Satisfied');
+    }
+    var funkyAndy, objectAndy;
+    beforeEach(function () {
+      funkyAndy = closurePerson('Funky Andy');
+      objectAndy = Object.create(protoPerson);
+      objectAndy._name = 'Object Andy';
+    });
+    it("when called synchronously", function () {
       testPersonState(funkyAndy);
-      //done(); // Mocha Error: done() called multiple times
-    }, 10);
-    var objectAndy = Object.create(protoPerson);
-    objectAndy._name = 'Object Andy';
-    setInterval(function () {
       testPersonState(objectAndy);
-      done();
-    }, 10);
+    });
+    it("when called asynchronously", function (done) {
+      setInterval(function () {
+        testPersonState(funkyAndy);
+        //done(); // Mocha Error: done() called multiple times
+      }, 10);
+      setInterval(function () {
+        testPersonState(objectAndy);
+        done();
+      }, 10);
+    });
+    it("when chained together as promises", function (done) {
+      function setIntervalWithPromise(person) {
+        setInterval(function () {
+          testPersonState(person);
+          done(); // Mocha Error: done() called multiple times
+        }, 10);
+      }
+      setIntervalWithPromise(funkyAndy);
+      setIntervalWithPromise(objectAndy);
+    });
+  });
+  describe("successful promise resolution with mocha and chai", function () {
+    var cadey;
+    beforeEach(function () {
+      cadey = closurePerson('Cadey');
+      //cadey = Object.create(protoPerson);
+      //cadey._name = 'Cadey'; // this is not bound correctly when promise calls back: Cannot set property '_disposition' of undefined
+    });
+    it('should illustrate use terminating of promise chains with done - done for promise fulfillment', function (done) {
+      expect(cadey.disposition()).to.equal('Anticipation');
+
+      var pizzaOrderFulfillment = q.defer();
+      var pizzaDelivered = pizzaOrderFulfillment.promise;
+
+      //pizzaDelivered.then(cadey.eat, cadey.beHungry)
+      //  .done(function () {
+      //    console.log("Done Called...");
+      //    done(); // double done's triggered somehow from this test which mocha somehow ran twice!!
+      //  });
+
+      pizzaDelivered.then(cadey.eat, cadey.beHungry)
+        .then(function (message) {
+          expect(message).to.exist;
+          expect(message).to.have.string('Pepperoni');
+        })
+        .done(done); // done reports Uncaught AssertionError: expected undefined to exist, expectations fail above
+      // This style encourages one to catch AssertionErrors, let's see how the others handle non expectation failures?!
+
+      //expect(cadey.disposition()).to.equal('Satisfied'); // Cmd line quiting before this...
+
+      pizzaOrderFulfillment.resolve('Pepperoni');
+    });
+    it('should illustrate returning our promises to mocha to await fulfillment', function () {
+      expect(cadey.disposition()).to.equal('Anticipation');
+
+      var pizzaOrderFulfillment = q.defer();
+      var pizzaDelivered = pizzaOrderFulfillment.promise;
+
+      pizzaOrderFulfillment.resolve('Pepperoni'); // This return style relies on the fact you can resolve() before then() with promises
+
+      return pizzaDelivered.then(cadey.eat, cadey.beHungry)
+        .then(function(message) {
+          expect(message).to.exist;
+          expect(message).to.have.string('Pepperoni');
+        }); // Mocha will just say AssertionError: expected undefined to exist
+    });
+    it('should illustrate use of chai-as-promised to notify of promise fulfillment', function (done) {
+      expect(cadey.disposition()).to.equal('Anticipation');
+
+      var pizzaOrderFulfillment = q.defer();
+      var pizzaDelivered = pizzaOrderFulfillment.promise;
+      expect(pizzaDelivered.then(cadey.eat, cadey.beHungry)) // Can only set up a single expectation with this concise style
+        .to.eventually.have.string('Pepperoni') // Get stack with have string and not with exist above??
+        // TODO try compound expectation eg. expect(fn).to.throw(ReferenceError) .and.not.throw(/good function/);
+        .notify(done);
+
+      pizzaOrderFulfillment.resolve('Pepperoni');
+    });
+  });
+  describe("promise rejection with mocha and chai", function () {
+    var cadey;
+    beforeEach(function () {
+      cadey = closurePerson('Cadey');
+    });
+    it('should illustrate use terminating of promise chains with done - done for promise fulfillment', function (done) {
+      expect(cadey.disposition()).to.equal('Anticipation');
+
+      var pizzaOrderFulfillment = q.defer();
+      var pizzaDelivered = pizzaOrderFulfillment.promise;
+
+      pizzaDelivered.then(cadey.eat, cadey.beHungry)
+        // Originally had my reject handler above return value, rather than throwing exception!
+        // The well meaning but superflous then() below succeeded in being resolved with my error message,
+        // which was of course re-thrown as an AssertionExcepton with err.actual set, then caught
+
+        //.then(function (message) {
+        //  expect(message).not.to.exist;
+        //})
+        .catch(function (err) {
+          expect(err).to.exist;
+          expect(err).to.be.an.instanceOf(Error);
+          expect(err.message).to.have.string('menu');
+        })
+        .done(done);
+
+      pizzaOrderFulfillment.reject("Couldn't find take out menu!");
+    });
+    it('should illustrate returning our promises to mocha to await fulfillment', function () {
+      expect(cadey.disposition()).to.equal('Anticipation');
+
+      var pizzaOrderFulfillment = q.defer();
+      var pizzaDelivered = pizzaOrderFulfillment.promise;
+
+      pizzaOrderFulfillment.reject("Couldn't find take out menu!");
+
+      return pizzaDelivered.then(cadey.eat, cadey.beHungry)
+        .catch(function (err) {
+          expect(err).to.exist;
+          expect(err).to.be.an.instanceOf(Error);
+          expect(err.message).to.have.string('menu');
+        });
+    });
+    it('should illustrate use of chai-as-promised to notify of promise fulfillment', function (done) {
+      expect(cadey.disposition()).to.equal('Anticipation');
+
+      var pizzaOrderFulfillment = q.defer();
+      var pizzaDelivered = pizzaOrderFulfillment.promise;
+
+      pizzaOrderFulfillment.reject("Couldn't find take out menu!");
+
+      expect(pizzaDelivered.then(cadey.eat, cadey.beHungry)) // Can only set up a single expectation with this concise style
+        .to.be.rejected
+        //.to.be.rejectedWith(/menu/)
+        //.to.be.rejectedWith("\n\nCadey is hungry because: Couldn't find take out menu!")
+        .notify(done);
+
+      // CwP carefully showed that my cadey error handler is returning a value, not an error, so promise chain is not rejected :)
+      // AssertionError: expected promise to be rejected but it was fulfilled with '\n\nCadey is hungry because: Couldn\'t find take out menu!'
+    });
   });
 
-  describe.only('test q', function () {
-    describe("successful promise resolution with mocha and chai", function () {
-      var cadey;
-      beforeEach(function () {
-        cadey = closurePerson('Cadey');
-        //cadey = Object.create(protoPerson);
-        //cadey._name = 'Cadey'; // this is not bound correctly when promise calls back: Cannot set property '_disposition' of undefined
-      });
-      it('should illustrate use terminating of promise chains with done - done for promise fulfillment', function (done) {
-        expect(cadey.disposition()).to.equal('Anticipation');
-
-        var pizzaOrderFulfillment = q.defer();
-        var pizzaDelivered = pizzaOrderFulfillment.promise;
-
-        //pizzaDelivered.then(cadey.eat, cadey.beHungry)
-        //  .done(function () {
-        //    console.log("Done Called...");
-        //    done(); // double done's triggered somehow from this test which mocha somehow ran twice!!
-        //  });
-
-        pizzaDelivered.then(cadey.eat, cadey.beHungry)
-          .then(function (message) {
-            expect(message).to.exist;
-            expect(message).to.have.string('Pepperoni');
-          })
-          .done(done); // done reports Uncaught AssertionError: expected undefined to exist, expectations fail above
-        // This style encourages one to catch AssertionErrors, let's see how the others handle non expectation failures?!
-
-        //expect(cadey.disposition()).to.equal('Satisfied'); // Cmd line quiting before this...
-
-        pizzaOrderFulfillment.resolve('Pepperoni');
-      });
-      it('should illustrate returning our promises to mocha to await fulfillment', function () {
-        expect(cadey.disposition()).to.equal('Anticipation');
-
-        var pizzaOrderFulfillment = q.defer();
-        var pizzaDelivered = pizzaOrderFulfillment.promise;
-
-        pizzaOrderFulfillment.resolve('Pepperoni'); // This return style relies on the fact you can resolve() before then() with promises
-
-        return pizzaDelivered.then(cadey.eat, cadey.beHungry)
-          .then(function(message) {
-            expect(message).to.exist;
-            expect(message).to.have.string('Pepperoni');
-          }); // Mocha will just say AssertionError: expected undefined to exist
-      });
-      it('should illustrate use of chai-as-promised to notify of promise fulfillment', function (done) {
-        expect(cadey.disposition()).to.equal('Anticipation');
-
-        var pizzaOrderFulfillment = q.defer();
-        var pizzaDelivered = pizzaOrderFulfillment.promise;
-        expect(pizzaDelivered.then(cadey.eat, cadey.beHungry)) // Can only set up a single expectation with this concise style
-          .to.eventually.have.string('Pepperoni') // Get stack with have string and not with exist above??
-          // TODO try compound expectation eg. expect(fn).to.throw(ReferenceError) .and.not.throw(/good function/);
-          .notify(done);
-
-        pizzaOrderFulfillment.resolve('Pepperoni');
-      });
-    });
-    describe("promise rejection with mocha and chai", function () {
-      var cadey;
-      beforeEach(function () {
-        cadey = closurePerson('Cadey');
-      });
-      it('should illustrate use terminating of promise chains with done - done for promise fulfillment', function (done) {
-        expect(cadey.disposition()).to.equal('Anticipation');
-
-        var pizzaOrderFulfillment = q.defer();
-        var pizzaDelivered = pizzaOrderFulfillment.promise;
-
-        pizzaDelivered.then(cadey.eat, cadey.beHungry)
-          // Originally had my reject handler above return value, rather than throwing exception!
-          // The well meaning but superflous then() below succeeded in being resolved with my error message,
-          // which was of course re-thrown as an AssertionExcepton with err.actual set, then caught
-
-          //.then(function (message) {
-          //  expect(message).not.to.exist;
-          //})
-          .catch(function (err) {
-            expect(err).to.exist;
-            expect(err).to.be.an.instanceOf(Error);
-            expect(err.message).to.have.string('menu');
-          })
-          .done(done);
-
-        pizzaOrderFulfillment.reject("Couldn't find take out menu!");
-      });
-      it('should illustrate returning our promises to mocha to await fulfillment', function () {
-        expect(cadey.disposition()).to.equal('Anticipation');
-
-        var pizzaOrderFulfillment = q.defer();
-        var pizzaDelivered = pizzaOrderFulfillment.promise;
-
-        pizzaOrderFulfillment.reject("Couldn't find take out menu!");
-
-        return pizzaDelivered.then(cadey.eat, cadey.beHungry)
-          .catch(function (err) {
-            expect(err).to.exist;
-            expect(err).to.be.an.instanceOf(Error);
-            expect(err.message).to.have.string('menu');
-          });
-      });
-      it('should illustrate use of chai-as-promised to notify of promise fulfillment', function (done) {
-        expect(cadey.disposition()).to.equal('Anticipation');
-
-        var pizzaOrderFulfillment = q.defer();
-        var pizzaDelivered = pizzaOrderFulfillment.promise;
-
-        pizzaOrderFulfillment.reject("Couldn't find take out menu!");
-
-        expect(pizzaDelivered.then(cadey.eat, cadey.beHungry)) // Can only set up a single expectation with this concise style
-          .to.be.rejected
-          //.to.be.rejectedWith(/menu/)
-          //.to.be.rejectedWith("\n\nCadey is hungry because: Couldn't find take out menu!")
-          .notify(done);
-
-        // CwP carefully showed that my cadey error handler is returning a value, not an error, so promise chain is not rejected :)
-        // AssertionError: expected promise to be rejected but it was fulfilled with '\n\nCadey is hungry because: Couldn\'t find take out menu!'
-      });
-    });
-
-    var Restaurant = function () {
-      var currentOrder;
-      this.takeOrder = function (orderedItems) {
-        currentOrder = {
-          deferred: q.defer(),
-          items: orderedItems
-        };
-        return currentOrder.deferred.promise;
+  var Restaurant = function () {
+    var currentOrder;
+    this.takeOrder = function (orderedItems) {
+      currentOrder = {
+        deferred: q.defer(),
+        items: orderedItems
       };
-      this.deliverOrder = function () {
-        currentOrder.deferred.resolve(currentOrder.items);
-      };
-      this.problemWithOrder = function (reason) {
-        currentOrder.deferred.reject(reason);
-      };
+      return currentOrder.deferred.promise;
     };
+    this.deliverOrder = function () {
+      currentOrder.deferred.resolve(currentOrder.items);
+    };
+    this.problemWithOrder = function (reason) {
+      currentOrder.deferred.reject(reason);
+    };
+  };
 
-    it.skip('should illustrate promise rejection', function () {
-      var pizzaPit = new Restaurant();
-      var pizzaDelivered = pizzaPit.takeOrder('Capricciosa');
+  it.skip('should illustrate promise rejection', function () {
+    var pizzaPit = new Restaurant();
+    var pizzaDelivered = pizzaPit.takeOrder('Capricciosa');
 
-      pizzaDelivered.then(cadey.eat, cadey.beHungry);
+    pizzaDelivered.then(cadey.eat, cadey.beHungry);
 
-      pizzaPit.problemWithOrder('no Capricciosa, only Margherita');
-    });
+    pizzaPit.problemWithOrder('no Capricciosa, only Margherita');
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
