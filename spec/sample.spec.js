40c528eb1f55007dc4412486a8a38beb5383320a
awashbrook <awashbrook@gmail.com>
2015-04-21 07:32:12 +0100

    got github gists api example working

--- spec/sample.spec.js
+++ spec/sample.spec.js
@@ -9,8 +9,10 @@ var
   request = require('request'),
   soap = require('soap');
 
+var nockConfig = require('./nocks.spec');
 var config = require('../src/config');
 var discovery = require('../src/discovery');
+var github = require('../github.config');
 
 // https://github.com/poetic/nock-vcr-recorder#configuration
 //vcr = require('nock-vcr-recorder-mocha');
@@ -25,6 +27,53 @@ describe('sample', function () {
   describe('test internet endpoints', function () {
     it('github', function (done) {
       discovery.ping(done, 'https://github.com/');
+    });
+  });
+  describe.only('test gists', function () {
+    var gistOpts, starredGistOpts;
+    beforeEach(function () {
+      gistOpts = {
+        url: 'https://api.github.com/gists?access_token=' + github.gists.token,
+        json: true,
+        headers: {
+          'User-Agent': 'request'
+        }
+      };
+      starredGistOpts = {
+        url: 'https://api.github.com/gists/starred?access_token=' + github.gists.token,
+        json: true,
+        headers: {
+          'User-Agent': 'request'
+        }
+      };
+    });
+    it('listing my gists require auth', function (done) {
+      request(github.gists.url, function (error, response, body) {
+          expect(response.statusCode).to.equal(403);
+          console.log(body); // http://developer.github.com/v3/#user-agent-required
+          done();
+        }
+      );
+    });
+    it('list my gists with auth token', function (done) {
+      request(gistOpts, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          console.log(body);
+          console.log('Discovered ' + body.length + ' gists');
+          expect(body[0].url).to.equal('https://api.github.com/gists/566494c6108aa8ad0981');
+          done();
+        }
+      );
+    });
+    it('list my starred gists with auth token', function (done) {
+      request(starredGistOpts, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          console.log(body);
+          console.log('Discovered ' + body.length + ' gists');
+          expect(body[0].url).to.equal('https://api.github.com/gists/6614023');
+          done();
+        }
+      );
     });
   });
   describe('request', function() {
